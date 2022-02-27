fd00ca76df0fa3f619c232b7e03a4b419295ad09
Tammy <tammycollinse@gmail.com>
2022-02-27 09:06:31 -0500

    commit tree 44ac9e537ddddf326a5afc853190b51bf4e42c75

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Code your solution here
+//findMatching- This function takes an array of drivers' names and a string as arguments
+// and returns the matching list of drivers. The function should be case insensitive.
+
+
+//demo using the array.filter
+
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+// const result = words.filter(word => word.length > 6);
+
+// console.log(result);
+
+
+
+
+// function fuzzyMatch(item , letter){
+//  return item.charAt(0) === letter; // return item.startsWith(letter);
+// }
+
+// let matching = drivers.filter(function (name){
+//    if (item === name){
+//    console.log(item)
+//     return item;
+//    }
+
+// function findMatching(driver, name){
+//    return driver === name;
+// }
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+// let findMatching = function(driver, name){
+//    if(driver === name){
+//       return driver.filter(drivers);````
+//    }
+// }
+
+function findMatching(driver, matchTest){
+   if (driver === matchTest){
+      return driver
+   }
+  
+   //    return driver.filter(drivers);
+   // }
+}
+
+let arr = drivers.filter(findMatching(drivers, "Bobby"));
+
+
+// function findMatching drivers.filter(function(driver, name = "Bobby"){
+//       if(driver === name){
+//          return true;
+//       }
+// })
+
